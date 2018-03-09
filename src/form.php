3a6983df3d68e3ecb63830a133907f46061ecc34
Managersky <witoldmetel@gmail.com>
2018-03-09 17:54:19 +0100

    form php test

---
+++ src/form.php
@@ -0,0 +1,11 @@
+<?php
+
+    // $usernameRegisterForm = $_POST["username_register_form"];
+    // $passwordRegisterForm = $_POST["password_register_form"];
+    // $confirmPasswordRegisterForm = $_POST["confirm_password_register_form"];
+    // $emailRegisterForm = $_POST["email_register_form"];
+    // $locationRegisterForm = $_POST["location_register_form"];
+    // isset($_POST['register_checkbox']); //checkbox is checked
+
+    echo "hfdshdsfdsh";
+?>
